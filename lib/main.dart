fa167c67772e1ea3aae9357fb7952e647f7889ca
george <georgii.gabaidze@gmail.com>
2024-05-11 13:26:27 +0300

    initial setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ShoedStudent());
+}
+
+class ShoedStudent extends StatelessWidget {
+  const ShoedStudent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
